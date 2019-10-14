3947df4135385a1686550c7dd8ddad8da1bcc6ff
alimkarimi <32401390+alimkarimi@users.noreply.github.com>
2019-10-13 19:55:41 -0700

    Update gtest to having 5 different testse

--- googletest/gtest_example_alim.cc
+++ googletest/gtest_example_alim.cc
@@ -1,7 +1,25 @@
 #include "add_function.cc"
 #include "gtest/gtest.h"
+#include <string>
 
 TEST(AdditionTest, CanAddInts){
 EXPECT_EQ(add(1,1), 2);
-EXPECT_EQ(add(90, -100), -10); //Ellie
+
 }
+
+TEST(AdditionTest, CanAddDoubles){
+EXPECT_EQ(add(1.5, 5.2),6.7);}
+
+TEST(AdditionTest, CanSubtract){
+EXPECT_EQ(add(90,-100),-10);}
+
+TEST(AdditionTest, ValidatesLessThan){
+EXPECT_LT(add(2,4),9);
+}
+
+TEST(AdditionTest, ValidatesNotEquals){
+EXPECT_NE(add(0,0),1);
+}
+
+TEST(AdditionTest, FailureExample){
+EXPECT_NE(add(0,0),0);}
