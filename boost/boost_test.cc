8e5dcd05522a0a3436d673e1fe5b036817dcb174
luisAcme <56280741+luisAcme@users.noreply.github.com>
2019-10-13 22:02:37 -0700

    Update boost_test.cc

--- boost/boost_test.cc
+++ boost/boost_test.cc
@@ -1,138 +1,20 @@
 #include <iostream>
-#define BOOST_TEST_MODULE example
-#include <boost/test/included/unit_test.hpp>
-#include <boost/mpl/list.hpp>
+#define BOOST_TEST_MODULE example //Add this for start using BOOST
+#include <boost/test/included/unit_test.hpp> // Add this library to enter to the testing 
+#include <boost/mpl/list.hpp> // add this to start using the templates in the program
 
-//A prime number is known for been able to be divided by itself
-template <class T>
-T prime_numbers(T a)
-{ 
-  for (T i = 2; i < a; i++) 
-    if (a % i == 0) //this formula would try to find if the total is 0
-    {
-      return -1; //-1 means that the answer is not a prime number
-    }
-    else;
-    {
-      return 0; //0 means is a prime number
-    }
-  
-}
-
-
-
-
-//Test 1
-typedef boost::mpl::list<int> test1_types;
-
-BOOST_AUTO_TEST_CASE(test_prime)
-{
-  int random_number;
-  std::cout << "Enter a number:" << std::endl;
-  std::cin >> random_number;
-
-  BOOST_TEST( prime_numbers(random_number)  == (unsigned)0);
-}
-
-
-
-
-
-// test factorial
-template <class T>
-T factorial(T a)
-{
-  T factorial = 1;
-  for(T i = 1; i <=10; ++i)
-    {
-        factorial *= i;
-    }
-    std::cout << "10! = " << factorial;
-    return factorial;
-}
-
-//Test 2
-typedef boost::mpl::list<long> test2_types;
-
-BOOST_AUTO_TEST_CASE(exponential_of_10)
-{
-  BOOST_TEST( factorial(10)  == (unsigned)3628800);
-}
-
-
-
-
-
-//test equal name
-template <class T> //one template for name
-T search_Name(T a)
-{
-  T name;
-
-  std::cout << "Reenter your name:" << std::endl; //this will automatically appear after you enter the name for testing
-  std::cin >> name;
-
-  return name;
-}
-
-//Test 3
-typedef boost::mpl::list<std::string> test_Name;
-
-BOOST_AUTO_TEST_CASE(equal_Names)
-{
-  std::string info_Name;
-  std::cout << "enter the name:" << std::endl;
-  std::cin >> info_Name;
-
-
-  BOOST_TEST( search_Name(info_Name)  == (std::string)info_Name); // this will let you save your first name and rewrite the new name
-}
-
-
-
-
-//test equal ID
-template <class T> //template for the ID
-T search_ID(T a)
-{
-  T look_ID;
-
-  std::cout << "Reenter your ID:" << std::endl; //will appear after you enter the first time ID 
-  std::cin >> look_ID;
-
-  return look_ID;
-}
-
-//Test 4
-typedef boost::mpl::list<int> test_ID; //you need to create two different mlp so you are able to use int and std::string without having problems at compiling
-
-BOOST_AUTO_TEST_CASE(look_ID)
-{
-  int info_ID;
-  std::cout << "enter your ID:" << std::endl;
-  std::cin >> info_ID;
-
-  BOOST_TEST( search_ID(info_ID)  == (int)info_ID);
-
-}
-
-
-
-
-//test mid value
 template <class T>
 T mid_Value(T a, T b)
 {
-  T mid_value = (a+b)/2;//the function will do one by itself
-
-    std::cout << "The middle value of " << a << " and " << b <<  " is: " << mid_value << std::endl;
+  float mid_value = (a+b)/2;//the function will do one by itself
+  
+    std::cout << "The middle value of " << a << " and " << b <<  " is: " << mid_value << std::endl;    
     return mid_value;
 }
 
-//Test 5
-typedef boost::mpl::list<int,long,unsigned char> test_types;
+typedef boost::mpl::list<float> float_types;
 
-BOOST_AUTO_TEST_CASE_TEMPLATE( mid_test, T, test_types )
+BOOST_AUTO_TEST_CASE_TEMPLATE( mid_test, T, float_types )//this will work only for the float types
 {
   T number1, number2;
 
@@ -142,23 +24,66 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( mid_test, T, test_types )
   std::cout << "Enter the second number: " << std::endl;
   std::cin >> number2;
 
-  BOOST_TEST( mid_Value(number2, number1)  == (unsigned)((number1+number2)/2)); //the boost will create a middle sormula by itself and if the answer is the same as the function mid_Value then it will be no mistake
+  BOOST_TEST( mid_Value(number1, number2)  == (float)((number1+number2)/2)); //the boost will create a middle sormula by itself and if the answer is the same as the function mid_Value then it will be no mistake
 }
 
+typedef boost::mpl::list<short int> shortInt_types;
 
-
-
-//test add
-template <class T>
-T add(T a  , T b)
+BOOST_AUTO_TEST_CASE_TEMPLATE( mid2_test, T, shortInt_types ) //this will work only for the short int types
 {
-  return a+b;
+  T number1, number2;
+
+  std::cout << "Enter the first number: " << std::endl; 
+  std::cin >> number1;
+
+  std::cout << "Enter the second number: " << std::endl;
+  std::cin >> number2;
+
+  BOOST_TEST( mid_Value(number1, number2)  == (float)((number1+number2)/2)); //the boost will create a middle sormula by itself and if the answer is the same as the function mid_Value then it will be no mistake
 }
 
 
-typedef boost::mpl::list<long> long_type; //This will read any element in a long type
+typedef boost::mpl::list<double> double_types;
 
-BOOST_AUTO_TEST_CASE_TEMPLATE( searching_two_longs, T, long_type )
+BOOST_AUTO_TEST_CASE_TEMPLATE( mid3_test, T, double_types ) //this will work only for the double types
 {
-  BOOST_TEST( add((sizeof(T)) ,(sizeof(T)) )  == (unsigned)16 ); //if the sum of two longs are not 16 it means that they are not long
+  T number1, number2;
+
+  std::cout << "Enter the first number: " << std::endl; 
+  std::cin >> number1;
+
+  std::cout << "Enter the second number: " << std::endl;
+  std::cin >> number2;
+
+  BOOST_TEST( mid_Value(number1, number2)  == (float)((number1+number2)/2)); //the boost will create a middle sormula by itself and if the answer is the same as the function mid_Value then it will be no mistake
 }
+
+typedef boost::mpl::list<long> long_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( mid4_test, T, long_types )//this will work only for the long types
+{
+  T number1, number2;
+
+  std::cout << "Enter the first number: " << std::endl; 
+  std::cin >> number1;
+
+  std::cout << "Enter the second number: " << std::endl;
+  std::cin >> number2;
+
+  BOOST_TEST( mid_Value(number1, number2)  == (float)((number1+number2)/2)); //the boost will create a middle sormula by itself and if the answer is the same as the function mid_Value then it will be no mistake
+}
+
+
+typedef boost::mpl::list<long long> longLong_types;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( mid5_test, T, longLong_types ) //this will work only for the long long types
+{
+  T number1, number2;
+
+  std::cout << "Enter the first number: " << std::endl; 
+  std::cin >> number1;
+
+  std::cout << "Enter the second number: " << std::endl;
+  std::cin >> number2;
+
+  BOOST_TEST( mid_Value(number1, number2)  == (float)((number1+number2)/2)); //the boost will create a middle sormula by itself and if the answer is the same as the function mid_Value then it will be no mistake
